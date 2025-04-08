480b584c4ab37ba4d293c94e68198be701e090ce
shriya31d <shriyad@microsoft.com>
2025-04-08 23:38:15 +0530

    updated .env file

--- trip_planner/src/trip_planner/crew.py
+++ trip_planner/src/trip_planner/crew.py
@@ -1,33 +1,18 @@
 import os
 from crewai import Agent, Crew, Process, Task,LLM
 from crewai.project import CrewBase, agent, crew, task
-
-# Uncomment the following line to use an example of a custom tool
-# from surprise_travel.tools.custom_tool import MyCustomTool
-
-# Check our tools documentation for more information on how to use them
 from crewai_tools import SerperDevTool, ScrapeWebsiteTool
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from azure.identity import DefaultAzureCredential
 from src.pydantic_models.SightseeingPlan import SightSeeingPlan
 from src.pydantic_models.TravelItinerary import TravelItinerary
 from src.tools.custom_tool import add_actual_dates_in_itinerary
-from langchain_openai import ChatOpenAI, AzureChatOpenAI
 
 @CrewBase
 class TripPlannerCrew():
     """TripPlanner crew"""
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
-
-    credential = DefaultAzureCredential()
-    azure_llm = AzureChatOpenAI(
-        api_version= "2023-05-15",
-        azure_endpoint="https://<replace>.openai.azure.com/",
-        deployment_name="gpt-4o",
-        azure_ad_token = credential.get_token("https://cognitiveservices.azure.com/.default").token
-    )
     
     @agent
     def destination_expert(self) -> Agent:
@@ -65,8 +50,7 @@ class TripPlannerCrew():
         return Task(
             config=self.tasks_config['itinerary_compilation_task'],
             agent=self.itinerary_compiler(),
-            output_json=TravelItinerary,
-            max_iter = 5
+            output_json=TravelItinerary
         )
 
     @crew
@@ -77,6 +61,5 @@ class TripPlannerCrew():
             tasks=self.tasks, # Automatically created by the @task decorator
             verbose = True,
             process=Process.sequential,
-            # manager_agent= self.manager,
-            llm = self.azure_llm
+            # manager_agent= self.manager
         )
